c7f2dc9bc6a6c9f08d20afd677680fd77b9f8139
jmurphy1808 <jmurphy1808@gmail.com>
2026-03-22 17:35:18 -0400

    Module 3 static HTML converted to HBS templates with JSON

--- app_server/controllers/travlrControllers.js
+++ app_server/controllers/travlrControllers.js
@@ -1,6 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+
+const tripsPath = path.join(__dirname, "..", "data", "trips.json");
+const trips = JSON.parse(fs.readFileSync(tripsPath, "utf8"));
+
 const travel = (req, res) => {
   res.render("travel", {
     title: "Travlr Getaways",
+    trips,
   });
 };
 
